7722b56da9d6f79ccccbf20d8f2885c4db2d6745
Mo-BiuS <lodland95@gmail.com>
2025-01-30 10:56:44 +0100

    handle player disconnect from game

--- Source/Game/Player/PlayerHandler.gd
+++ Source/Game/Player/PlayerHandler.gd
@@ -3,12 +3,19 @@ class_name PlayerHandler extends Node
 @onready var game:Game = $".."
 var playerPacked:PackedScene = preload("res://Source/Game/Player/Player.tscn")
 
+func _ready() -> void:
+	multiplayer.peer_disconnected.connect(removePlayer)
 
 func addPlayer(id:int)->Player:
 	var player:Player = playerPacked.instantiate()
 	player.id = id
 	add_child(player,true)
 	return player
+
+func removePlayer(id:int)->void:
+	for i in get_children():
+		if i is Player && i.id == id:i.queue_free()
 	
+
 func _on_character_body_3d_changed_chunk(posX: int, posY: int) -> void:
 	game.changedChunk(posX,posY)
